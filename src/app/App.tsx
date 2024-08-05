f92862f41e055b3c9df9d7719b22cdeb9a9636eb
Wojciech Wiśniewski <wojciech.wisniewski@goshop.pl>
2024-08-05 16:19:36 +0200

    Added styled components & theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,1 +1,14 @@
-export const App = () => <div>Hello World!!!</div>
+import styled, { ThemeProvider } from 'styled-components'
+import { theme } from '../lib/styles'
+
+export const App = () => (
+  <ThemeProvider theme={theme}>
+    <AppContainer>Hello World!!!</AppContainer>
+  </ThemeProvider>
+)
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.background};
+`
